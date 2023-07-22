9f379d6abf8bb0dd8ae4c15d44dbd960e227879b
Đức Mạnh <ducmanhlai22@gmail.com>
2023-07-22 23:06:52 +0700

    dfs

--- src/public/js/createOrder.js
+++ src/public/js/createOrder.js
@@ -1,4 +1,17 @@
 import axios from "./axios.js";
+import { io } from "https://cdn.socket.io/4.4.1/socket.io.esm.min.js";
+const socket = io();
+socket.on('connect', function () {
+    socket.emit('authenticate', { token: localStorage.getItem('accessToken') });
+    socket.on('err', (err) => {
+        window.location.href = '/view/login'
+    })
+});
+socket.on("disconnect", () => {
+    console.log(socket.id);
+});
+document.getElementById('btn-add').addEventListener('click',createOrder)
+const listOrder = [];
 (async () => {
     const typeList = (await axios.get('/api/v1/type/get')).data.data
     // Tạo danh sách các loại món ăn
@@ -6,23 +19,45 @@ import axios from "./axios.js";
     typeList.forEach(function (food) {
         const listItem = document.createElement('button');
         listItem.setAttribute('class', 'btn btn-labeled btn-info mr-3')
-        listItem.innerText = food.name; // Giả sử 'name' là thuộc tính chứa tên món ăn
+        listItem.innerText = food.name;
         typeListElement.appendChild(listItem);
     });
 })().catch(function (error) {
     console.error('Không thể lấy danh sách loại', error);
 });
-(async ()=>{
+function addFood(food) {
+    listOrder.push({ id: food.id, quantity: 1, price: food.price })
+}
+function createOrder() {
+    const order = {
+        "id_staff": 2,
+        "table": 21,
+        "note": "Bỏ ít ớt thôi, không cho đườngg",
+        'detail': [...listOrder]
+    }
+    console.log(order)
+}
+(async () => {
     const listFood = (await axios.get('/api/v1/food/get')).data.data
-    // Tạo danh sách các loại món ăn
+
     const foodListElement = document.getElementById('listFood');
     listFood.forEach(function (food) {
         const listItem = document.createElement('div');
-        listItem.setAttribute('class',"container-fluid")
-        listItem.innerHTML =  `<div class="shadow p-3 mb-1 bg-white rounded row">
-                 <p class='col-10'>${food.name}</p>
-                 <button class='col'>Thêm</button>
-        </div>`
+        listItem.setAttribute('class', "container-fluid")
+        const div= document.createElement('div')
+        div.setAttribute('class','shadow p-3 mb-1 bg-white rounded row')
+        div.innerHTML = `
+                 <p class='col-10'>${food.name}</p>`
+       
+        const buttonElement = document.createElement('button');
+        buttonElement.setAttribute('id',food.id)
+        buttonElement.setAttribute('class','col')
+        buttonElement.innerText = 'Thêm'
+        buttonElement.addEventListener('click',(ev)=>{
+            addFood(food)
+        })
+        div.appendChild(buttonElement)
+        listItem.appendChild(div)
         foodListElement.appendChild(listItem);
     });
 })().catch(function (error) {
